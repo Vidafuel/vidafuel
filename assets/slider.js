3c677f2c11905d1d91b1de09773b3e32470112f5
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-03-16 18:23:13 -0700

    dev theme pull

---
+++ assets/slider.js
@@ -0,0 +1,13 @@
+var elem = document.querySelector(".main-carousel")
+var flkty = new Flickity(elem, {
+	// options
+	contain: true,
+	draggable: true,
+	wrapAround: true,
+})
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity(".main-carousel", {
+	// options
+})
